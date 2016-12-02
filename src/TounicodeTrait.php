3604e9c2f80d81836a5a2287203bd5275bb31960
kknts <kyawnainghtun93@gmail.com>
2016-12-02 22:37:44 +0630

    added language detector

--- src/TounicodeTrait.php
+++ src/TounicodeTrait.php
@@ -9,13 +9,31 @@ trait TounicodeTrait{
    * @return [string]        [output unicode]
    */
   public function toUnicode($value){
-   // perform a case-Insensitive search for the word "Vi"
-  //  $regex = "/\s\1031| ေ[က-အ]်|[က-အ]း/i";
-  //  if (preg_match($regex, $value, $match)) :
-  //     print "Match found!";
-  //   endif;
+    $inputValue = $this->testZgOrUni($value);
+    if($inputValue == "ZawGyi") {
+      return $this->replaceWithRule($value);
+    }else{
+      return $value;//if others, return origin value
+    }
+  }
+  /**
+   * [defineUnicodeRule description]
+   * @param  [string] $value [user's input value]
+   * @return [string]        [output result]
+   */
+  protected static function testZgOrUni($value){
+      $regexUni = '/[ဃငဆဇဈဉညဋဌဍဎဏဒဓနဘရဝဟဠအ]်|ျ[က-အ]ါ|ျ[ါ-း]|\x{103e}|\x{103f}|\x{1031}[^\x{1000}-\x{1021}\x{103b}\x{1040}\x{106a}\x{106b}\x{107e}-\x{1084}\x{108f}\x{1090}]|\x{1031}$|\x{1031}[က-အ]\x{1032}|\x{1025}\x{102f}|\x{103c}\x{103d}[\x{1000}-\x{1001}]|ည်း|ျင်း|င်|န်း|ျာ|င့်/u';
+      $regexZG = '/\s\x{1031}| ေ[က-အ]်|[က-အ]း/u';
+      $regexMM = '/[\x{1000}-\x{109f}\x{aa60}-\x{aa7f}]+/u';
 
-    return $this->replaceWithRule($value);
+      if(preg_match($regexUni,$value))
+          return "Unicode";
+      else if(preg_match($regexZG,$value))
+          return "ZawGyi";
+      else if(preg_match($regexMM,$value))
+          return "Myanmar";
+      else
+          return "NotMyanmar";
   }
 
   /**
